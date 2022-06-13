a04576342632d9d5144272c9d7f221c575cd65fe
Junaeid18 <mdjunaeid98@gmail.com>
2022-06-14 04:25:25 +0600

    data read from database using jdbc rsultset

--- JDBC_Starter/src/main/java/com/junaeid/jdbc/dao/Account_dao.java
+++ JDBC_Starter/src/main/java/com/junaeid/jdbc/dao/Account_dao.java
@@ -3,6 +3,7 @@ package com.junaeid.jdbc.dao;
 import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Account_dao {
@@ -13,9 +14,20 @@ public class Account_dao {
 		Statement st = conn.createStatement();
 		//int result = st.executeUpdate("insert into account values(1003,'Ziaul','Hasan',7000)");
 		//System.out.println(result+" rows got inserted");
+		
 		//int result = st.executeUpdate("update account set balance = 15940 where accno = 1001");
 		//System.out.println(result+" rows got updated");
-		int result = st.executeUpdate("delete from account where accno between 1002 and 1003");
-		System.out.println(result+" rows got deleted");
+		
+		//int result = st.executeUpdate("delete from account where accno between 1002 and 1003");
+		//System.out.println(result+" rows got deleted");
+		
+		ResultSet res = st.executeQuery("select * from account");
+		while(res.next()) {
+			int acc = res.getInt(1);
+			String fname = res.getString(2);
+			String lname = res.getString(3);
+			int bala = res.getInt(4);
+			System.out.println(acc+" "+fname+" "+lname+" "+bala);
+		}
 	}
 }
