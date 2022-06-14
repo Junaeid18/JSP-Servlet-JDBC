42e03d896eb8354796ad5dfb9a979ff7da88d83a
Junaeid18 <mdjunaeid98@gmail.com>
2022-06-15 02:24:35 +0600

    UserApp initialized and users table created in sqlserver

---
+++ UserApp/userApp.sql
@@ -0,0 +1,16 @@
+create database mydb;
+
+use mydb;
+
+create table users(
+	firstName varchar(20),
+	lastName varchar(20),
+	email varchar(30),
+	password varchar(20)
+);
+
+select * from  users;
+
+
+
+
