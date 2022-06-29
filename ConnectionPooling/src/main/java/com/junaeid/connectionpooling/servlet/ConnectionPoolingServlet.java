7619b79c9ca686e7af5de3b1969825f36c04c111
Junaeid18 <mdjunaeid98@gmail.com>
2022-06-30 03:39:32 +0600

    Connection Pooling Demo done

---
+++ ConnectionPooling/src/main/java/com/junaeid/connectionpooling/servlet/ConnectionPoolingServlet.java
@@ -0,0 +1,29 @@
+package com.junaeid.connectionpooling.servlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+
+import javax.naming.InitialContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+@WebServlet("/ConnectionPoolingServlet")
+public class ConnectionPoolingServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			InitialContext initialContext = new InitialContext();
+			DataSource ds = (DataSource) initialContext.lookup("java:com/comp/env/myds");
+			Connection connection = ds.getConnection();
+			System.out.println(connection);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
