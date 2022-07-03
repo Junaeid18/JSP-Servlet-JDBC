3321192a20200539097f60a72911425a3b403f40
Junaeid18 <mdjunaeid98@gmail.com>
2022-07-04 05:01:50 +0600

    addProduct method finalized in productDAO

--- CouponAndProductApp/src/main/java/com/junaeid/candp/dao/ProductDAO.java
+++ CouponAndProductApp/src/main/java/com/junaeid/candp/dao/ProductDAO.java
@@ -1,5 +1,23 @@
 package com.junaeid.candp.dao;
 
-public class ProductDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import com.junaeid.candp.model.Product;
+import com.junaeid.candp.util.ConnectionUtil;
+
+public class ProductDAO {
+	public void addProduct(Product p) {
+		Connection conn = ConnectionUtil.getConnection();
+		try {
+			PreparedStatement pst = conn.prepareStatement("insert into products(name,desciption,price) values(?,?,?)");
+			pst.setString(1, p.getName());
+			pst.setString(2, p.getDescription());
+			pst.setBigDecimal(3, p.getPrice());
+			pst.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
