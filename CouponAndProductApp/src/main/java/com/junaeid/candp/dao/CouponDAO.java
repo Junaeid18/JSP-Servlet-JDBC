e3dd1905f896466f82c51eb6d99a13ad4f06a280
Junaeid18 <mdjunaeid98@gmail.com>
2022-07-04 03:35:58 +0600

    CreateCoupon method finalized in couponDao

--- CouponAndProductApp/src/main/java/com/junaeid/candp/dao/CouponDAO.java
+++ CouponAndProductApp/src/main/java/com/junaeid/candp/dao/CouponDAO.java
@@ -1,5 +1,23 @@
 package com.junaeid.candp.dao;
 
-public class CouponDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import com.junaeid.candp.model.Coupon;
+import com.junaeid.candp.util.ConnectionUtil;
+
+public class CouponDAO {
+	public void CreateCoupon(Coupon cp) {
+		Connection conn = ConnectionUtil.getConnection();
+		try {
+			PreparedStatement pst = conn.prepareStatement("insert into coupon(code,exp_date,discount) values(?,?,?)");
+			pst.setString(1, cp.getCode());
+			pst.setString(2, cp.getExp_date());
+			pst.setBigDecimal(3, cp.getDiscount());
+			pst.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
